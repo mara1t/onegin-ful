db3df35ff2e83af5b0ca12e295596420d8bfefef
mara1t <88665544+mara1t@users.noreply.github.com>
2021-08-19 16:33:12 +0500

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+float two_sol();
+
+float one_sol();
+
 int main()
 {
-    float a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+    float a = 0, b = 0, c = 0, x1 = 0, x2 = 0, x = 0;
+
+    float D = b * b - 4 * a * c;
+
     printf("Write down coefficient a b c from your equation ax^2+bx+c=0\n"
            "a:");
     scanf("%f", &a);
@@ -20,25 +27,37 @@ int main()
             printf("No solution");
         }
         if (b != 0){
-            x1 = -c / b;
-            printf("x=%.2f", x1);
+            one_sol();
+            printf("x=%.2f", x);
         }
     }
 // 2 case of 3, D<0, a!=0
     if(a != 0){
-
-        float D = b * b - 4 * a * c;
-
         if (D < 0){
             printf("No solution");
         }
 // 3 case of 3, a!=0 D>=0
 
         if (D >= 0){
-            x1 = ( -b + sqrt(D) ) / (2*a);
-            x2 = ( -b - sqrt(D) ) / (2*a);
+            two_sol();
             printf("x1=%.2f\nx2=%.2f", x1, x2);
         }
     }
     return 0;
 }
+
+float two_sol()
+{
+    float  x1 = 0, x2 = 0, D;
+    float a, b, c;
+    x1 = ( -b + sqrt(D) ) / (2*a);
+    x2 = ( -b - sqrt(D) ) / (2*a);
+}
+
+float one_sol()
+{
+    float D, x = 0;
+    float b, c;
+    x=-c / b;
+}
+
