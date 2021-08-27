41e0a9e0ca1fb6f24998d06d348bd43c595b84bd
mara1t <88665544+mara1t@users.noreply.github.com>
2021-08-27 22:56:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 
 int find_sol(double a, double b, double c, double *x1, double *x2);
-
+int inputing(double *a, double *b,double *c);
 
 int main()
 {
-    float a = 0, b = 0, c = 0, x1 = 2, x2 = 0, x = 0;
+    float a = 0, b = 0, c = 0, x1 = 2, x2 = 0;
 
-    float D = b * b - 4 * a * c;
-
-    printf("Write down coefficient a b c from your equation ax^2+bx+c=0\n"
-           "a:");
-    scanf("%f", &a);
-    printf("b:");
-    scanf("%f", &b);
-    printf("c:");
-    scanf("%f", &c);
-
-    switch(find_sol(a, b, c, &x1, &x2))
+    inputing(&a, &b, &c);
+    int num_of_roots=find_sol(a, b, c, &x1, &x2);
+    switch(num_of_roots)
     {
     case 0: printf("No solution\n");
             break;
@@ -33,32 +25,44 @@ int main()
 
 int find_sol(double a, double b, double c, double *x1, double *x2)
 {
-    double D = b*b - 4*a*c;
-    if(a==0){
-        if(b==0){
-            if(c==0){
+    double Discr = b*b - 4*a*c;
+    if (a == 0) {
+        if (b == 0) {
+            if (c == 0) {
                 return 3;
             }
-            else{
+            else {
                 return 0;
             }
         }
-        else{
-            *x1=-c/b;
+        else {
+            *x1 = -c / b;
             return 1;
         }
     }
-    else if(D == 0){
-        *x1=-b/(2*a);
+    else if (Discr == 0) {
+        *x1 = -b / (2*a);
         return 1;
     }
-    else if(D != 0){
-
-    *x1 = ( -b + sqrt(D) ) / (2*a);
-    *x2 = ( -b - sqrt(D) ) / (2*a);
+    else if (Discr != 0) {
+    double Sq_Di = sqrt(Discr);
+    *x1 = ( -b + Sq_Di ) / (2*a);
+    *x2 = ( -b - Sq_Di ) / (2*a);
     return 2;
     }
 }
+int inputing(double *a, double *b,double *c)
+{
+    printf("Write down coefficient a b c from your equation ax^2+bx+c=0\n"
+           "a:");
+    scanf("%f", &a);
+    printf("b:");
+    scanf("%f", &b);
+    printf("c:");
+    scanf("%f", &c);
+    return 0;
+}
+/*int one_sol()
+{
 
-
-
+}*/
