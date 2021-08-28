170b304133a855df0d93231c15a0fc20df2628e8
mara1t <88665544+mara1t@users.noreply.github.com>
2021-08-28 10:53:49 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int find_sol(double a, double b, double c, double *x1, double *x2);
-int inputing(double *a, double *b,double *c);
+int find_sol (double a, double b, double c, double *x1, double *x2);
+int inputing (double *a, double *b,double *c);
+
+const int INF_ROOTS = 3;
 
 int main()
 {
@@ -9,9 +11,10 @@ int main()
 
     inputing(&a, &b, &c);
 
-    int num_of_roots=find_sol(a, b, c, &x1, &x2);
 
-    switch(num_of_roots)
+    int num_of_roots = find_sol (a, b, c, &x1, &x2);
+
+    switch (num_of_roots)
     {
     case 0: printf("No solution\n");
             break;
@@ -19,43 +22,60 @@ int main()
             break;
     case 2: printf("x1=%lg\nx2=%g\n", x1, x2);
             break;
-    case 3: printf("many solution\n");
+    case 3 : printf("many solution\n");
             break;
     default:break;
     }
-    printf("%lg %lg %lg", a,b,c);
+   // printf("%lg %lg %lg", a,b,c);
     return 0;
 }
 
 int find_sol(double a, double b, double c, double* x1, double* x2)
 {
     double Discr = b * b - 4*a*c;
-    if (a == 0) {
-        if (b == 0) {
-            if (c == 0) {
-                return 3;
+    if (a == 0)
+    {
+        if (b == 0)
+        {
+            if (c == 0)
+            {
+                return INF_ROOTS;
             }
-            else {
+            else
+            {
                 return 0;
             }
         }
-        else {
+        else
+        {
             *x1 = -c / b;
             return 1;
         }
     }
-    else if (Discr == 0) {
+    else if (Discr == 0)
+    {
         *x1 = -b / (2*a);
         return 1;
     }
-    else if (Discr < 0) {
+    else if (Discr < 0)
+    {
         return 0;
     }
-    else if (Discr > 0) {
-        double Sq_Di = sqrt(Discr);
-        *x1 = ( -b + Sq_Di ) / (2*a);
-        *x2 = ( -b - Sq_Di ) / (2*a);
-        return 2;
+    else if (Discr > 0)
+    {
+        if (c == 0)
+        {
+            *x1 = 0;
+            *x2 = -b / a;
+            return 2;
+        }
+        else
+        {
+            double Sq_Di = sqrt(Discr);
+            *x1 = ( -b + Sq_Di ) / (2*a);
+            *x2 = ( -b - Sq_Di ) / (2*a);
+            return 2;
+        }
     }
 }
 int inputing(double* a, double* b,double* c)
