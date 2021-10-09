42281a023949926230a9573e08a88b63dc97cdcf
mara1t <88665544+mara1t@users.noreply.github.com>
2021-10-10 01:38:46 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
-
 #include <stdio.h>
+#include <assert.h>
 
+/** \brief find_roots() is needed to find roots from giving parameters
+ *
+ * \param 'a' 'b' 'c' param. form equation 'ax^2+bx+c=0'
+ * \param x1 and x2 solutions of equation if they exist
+ * \return returns number of roots
+ *
+ */
 int find_roots (double a, double b, double c, double *x1, double *x2);
+
 int inputs (double *a, double *b, double *c);
 double discriminant(double a, double b, double c);
-int more_than_0(double a);
+int mod_more_0(double a);
 int solve_linear(double k, double b, double* x1);
-int output_roots(double a, double b, double c, double* x1, double* x2);
+int output_roots (int num_of_roots, double x1, double x2);
 int test_project ();
 int check_input (double* a);
 
@@ -26,32 +34,35 @@ int main()
 {
     test_project();
 
-    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
-
+    double a = 0, b = 0, c = 0;
     inputs (&a, &b, &c);
 
-    output_roots(a, b, c, &x1, &x2);
+    double x1 = 0, x2 = 0;
+
+    int num_of_roots = find_roots (a, b, c, &x1, &x2);
+    output_roots(num_of_roots, x1, x2);
 
     return 0;
 }
+
 /** \brief find_roots() is needed to find roots from giving parameters
  *
  * \param 'a' 'b' 'c' param. form equation 'ax^2+bx+c=0'
  * \param x1 and x2 solutions of equation if they exist
- * \return returns number of roots
+ * \return number of roots
  *
  */
 int find_roots (double a, double b, double c, double* x1, double* x2)
 {
-
-    if (!more_than_0(a))
+    assert (x1 != x2);
+    if (!mod_more_0(a))
     {
         return solve_linear (b, c, &x1);
     }
 
     double Discr = discriminant(a, b, c);
 
-    if (!more_than_0(Discr))
+    if (!mod_more_0(Discr))
     {
         *x1 = -b / (2*a);
         return ONE_ROOT;
@@ -62,7 +73,7 @@ int find_roots (double a, double b, double c, double* x1, double* x2)
     }
     else if (Discr > 0)
     {
-        if (!more_than_0(c))
+        if (!mod_more_0(c))
         {
             *x2 = 0;
             return solve_linear(a, b, &x1) + 1;
@@ -76,11 +87,10 @@ int find_roots (double a, double b, double c, double* x1, double* x2)
         }
     }
 }
+
 /** \brief inputs() writing words for undestanding function of programm
  *
  * \param a b c param. of equation to count from console
- * \param
- * \return 0 anyway
  *
  */
 int inputs (double* a, double* b, double* c)
@@ -94,10 +104,10 @@ int inputs (double* a, double* b, double* c)
     check_input (c);
     return 0;
 }
+
 /** \brief checking param. counting from console not to write a letter or symbols instead of number
  *
  * \param a param. is coefficient from equation
- * \param
  * \return 0 anyway
  *
  */
@@ -106,16 +116,18 @@ int check_input (double* a)
    while (scanf("%lg", a) == 0)
    {
        printf("pls enter right number\n");
-       while ( getchar() != '\n')
-      {;}
+       while (getchar() != '\n') {;}
        printf(":");
    }
+
+   while (getchar() != '\n') {;}
+
    return 0;
 }
+
 /** \brief counting discriminant while solving equation
  *
  * \param a b c from equation
- * \param
  * \return meaning of discriminant
  *
  */
@@ -133,9 +145,9 @@ double discriminant (double a, double b, double c)
  */
 int solve_linear (double k, double b, double* x1)
 {
-    if (!more_than_0(k))
+    if (!mod_more_0(k))
     {
-        if (!more_than_0(b))
+        if (!mod_more_0(b))
             return INF_ROOTS;
         else
             return ZERO_ROOTS;
@@ -150,11 +162,10 @@ int solve_linear (double k, double b, double* x1)
 /** \brief comparing with 0
  *
  * \param a is param which we are comparing
- * \param
- * \return 0 if a equal to 0, else 1
+ * \return 0 if 'a' equal to 0, else 1
  *
  */
-int more_than_0 (double a)
+int mod_more_0 (double a)
 {
     if (fabs(a) <= epsilon)
         return 0;
@@ -164,15 +175,12 @@ int more_than_0 (double a)
 
 /** \brief printing solutions of equation
  *
- * \param a b c to find roots
- * \param x1 x2 are wrote on console
- * \return 0
+ * \param num_of_roots
+ * \param x1, x2 are wrote on console
  *
  */
-int output_roots (double a, double b, double c, double* x1, double* x2)
+int output_roots (int num_of_roots, double x1, double x2)
 {
-    int num_of_roots = find_roots (a, b, c, x1, x2);
-
     switch (num_of_roots)
     {
     case ZERO_ROOTS:
@@ -180,12 +188,12 @@ int output_roots (double a, double b, double c, double* x1, double* x2)
         break;
 
     case ONE_ROOT:
-        printf("x = %lg\n", *x1);
+        printf("x = %lg\n", x1);
         break;
 
     case TWO_ROOTS:
         printf("x1 = %lg\n"
-               "x2 = %lg\n", *x1, *x2);
+               "x2 = %lg\n", x1, x2);
         break;
 
     case INF_ROOTS:
@@ -193,28 +201,36 @@ int output_roots (double a, double b, double c, double* x1, double* x2)
         break;
 
     default:
-
         break;
     }
     return 0;
 }
 
 /** \brief testing our project to find out errors
- *
- * \param -
- * \param -
- * \return number of errors
- *
+ * \return num of errors
  */
 int test_project ()
 {
     double x1 = 0, x2 = 0;
     int ERRORS = 0;
-    ERRORS += (find_roots(0, 0, 0,  &x1, &x2) != INF_ROOTS)? 1 : 0;
-    ERRORS += (find_roots(1, 2, 1,  &x1, &x2) != ONE_ROOT || (x1 != -1))? 1:0;
-    ERRORS += (find_roots(0, 0, 1,  &x1, &x2) != ZERO_ROOTS )? 1:0;
-    ERRORS += (find_roots(1, 5, 4,  &x1, &x2) != TWO_ROOTS || ( (x1 != -4 && x2 != -1) && (x2 != -4 && x1 != -1) ) )? 1:0;
-    ERRORS += (find_roots(1, 3, 2,  &x1, &x2) != TWO_ROOTS || ( (x1 != -1 && x2 != -2) && (x2 != -1 && x1 != -2) ) )? 1:0;
+    double solut[] = {INF_ROOTS, ONE_ROOT, ZERO_ROOTS, TWO_ROOTS, TWO_ROOTS};
+    struct koef{
+    double a; double b; double c;
+    };
+    struct koef* k = (struct koef*) calloc(5, sizeof(*k));
+    k[0].a = 0, k[0].b = 0, k[0].c = 0;
+    k[1].a = 1, k[1].b = 2, k[1].c = 1;
+    k[2].a = 0, k[2].b = 0, k[2].c = 1;
+    k[3].a = 1, k[3].b = 5, k[3].c = 4;
+    k[4].a = 1, k[4].b = 3, k[4].c = 2;
+    for (int i = 0; i <= 4; i++)
+    {
+        if (find_roots(k[i].a, k[i].b, k[i].c,  &x1, &x2) != solut[i])
+        {
+            ERRORS++;
+        }
+    }
+    free(k);
     printf("%d ERRORS\n", ERRORS);
     return 0;
 }
