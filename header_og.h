e7d3170c7719f7db38f5d9bb327d11af02ad3abd
mara1t <88665544+mara1t@users.noreply.github.com>
2021-10-15 01:19:55 +0300

    Update header_og.h

--- header_og.h
+++ header_og.h
@@ -1,15 +1,18 @@
-struct n_len_str {
-        int ilen;
-        char* ifirst_smb;
-    };
-int fl_size (FILE* og);
-void swaping (char** psymbols, int str);
-char* file_read (int file_size, FILE* og);
-void point_str (int file_size, char *symbols, char** first_smb, int* len_str);
-void print_txt (int n_str, struct n_len_str *mystr);
-int nstr (int file_size, char *symbols);
-int compare_begin (struct n_len_str *maratcool1, struct n_len_str *maratcool2);
-int compare_end (struct n_len_str *maratcool1, struct n_len_str *maratcool2);
-//void caloc_point (int n_str ,int* len_str, char** first_smb, struct n_len_str* mystr);
-void free_cal(struct n_len_str* mystr, char** first_smb, char *symbols, FILE* file_onegin);
-void copy_mass (struct n_len_str* mystr, int* len_str, char** first_smb, int n_str);
+struct Line {
+    int len;
+    char* str;
+};
+char* file_read (const int file_size, const FILE* og);
+
+int File_size (const FILE* og);
+int Nstr (const int file_size, const char *buffer);
+
+void line_pointer_len (const int file_size, char *buffer, struct Line* mystr);
+
+int compare_begin (const struct Line *stract1, const struct Line *stract2);
+int compare_end (const struct Line *stract1, const struct Line *stract2);
+
+void free_member(struct Line* mystr, char *buffer, FILE* file_onegin, FILE* file_output);
+void output_txt (const FILE* file_output, const int n_str, const struct Line* mystr);
+
+
