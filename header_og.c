2973a57e4d610c34a13f7a85edc67164fefd5ee3
mara1t <88665544+mara1t@users.noreply.github.com>
2021-10-15 01:19:07 +0300

    Update header_og.c

--- header_og.c
+++ header_og.c
@@ -7,7 +7,7 @@
 #include "header_og.h"
 #include <ctype.h>
 
-void free_cal(struct Line* mystr, char *buffer, FILE* file_onegin, FILE* file_output)
+void free_member(struct Line* mystr, char *buffer, FILE* file_onegin, FILE* file_output)
 {
     free(mystr);
     free(buffer);
@@ -15,7 +15,7 @@ void free_cal(struct Line* mystr, char *buffer, FILE* file_onegin, FILE* file_ou
     fclose(file_onegin);
 }
 
-int File_size (FILE* og)
+int File_size (const FILE* og)
 {
     assert(og != NULL);
 
@@ -28,15 +28,10 @@ int File_size (FILE* og)
     return size;
 }
 
-void swaping (char** first_smb, int str)
+char* file_read (const int file_size, const FILE* og)
 {
-    char* t = first_smb[str];
-    first_smb[str] = first_smb[str+1];
-    first_smb[str+1] = t;
-}
+    assert(og != NULL && file_size >= 1);
 
-char* file_read (int file_size, FILE* og)
-{
     char* buffer = (char *) calloc(file_size + 1, sizeof(char));
     if (buffer == NULL)
     {
@@ -46,8 +41,7 @@ char* file_read (int file_size, FILE* og)
 
     else
     {
-        //проверка
-        if (fread (buffer, sizeof(char), file_size, og) == NULL)
+        if (fread (buffer, sizeof(char), file_size, og) == 0) // ToDo: != file_sizen
         {
             printf("ERROR, No symbols in text file");
             abort();
@@ -57,14 +51,14 @@ char* file_read (int file_size, FILE* og)
     }
 }
 
-void line_pointer_len (int file_size, char *buffer, struct Line* mystr)
+void line_pointer_len (const int file_size, char *buffer, struct Line* mystr)
 {
-  
+    assert(buffer != NULL && file_size >= 1 && mystr != NULL);
     int symb_counter = 0, str = 0, len_counter = 0;
     for ( str = 0; symb_counter < file_size; str++)
     {
         len_counter = 0;
-        mystr[str].str = &buffer[symb_counter];
+        mystr[str].str = &buffer[symb_counter]; // (&buf[symb] = &*(buf + symb) = buf + symb
         while ( buffer[symb_counter] != '\n' && symb_counter < file_size)
         {
             symb_counter++;
@@ -78,9 +72,9 @@ void line_pointer_len (int file_size, char *buffer, struct Line* mystr)
     }
 }
 
-int Nstr (int file_size, char *buffer)
+int Nstr (const int file_size, const char *buffer)
 {
-    
+    assert(buffer != NULL && file_size >= 1);
     int symb_num = 0, n_str = 0, str_num = 0;
     for (str_num = 0; symb_num < file_size; str_num++)
     {
@@ -94,21 +88,9 @@ int Nstr (int file_size, char *buffer)
     return n_str;
 }
 
-char** get_string(int n_str, FILE* file_onegin)
+void output_txt (const FILE* file_output, const int n_str, const struct Line* mystr)
 {
-    char** mystring = (char**) calloc(n_str, 40);
-
-    for(int str = 0; str < n_str; str++)
-    {
-        fgets(mystring[str], 40, file_onegin);
-    }
-
-    return mystring;
-}
-
-void output_txt (FILE* file_output, int n_str, struct Line* mystr)
-{
-  
+    assert(file_output != NULL && n_str >= 1 && mystr != NULL);
     for (int str_num = 0; str_num < n_str; str_num++)
     {
         if (*(mystr[str_num].str) != '\0')
@@ -125,27 +107,14 @@ void output_txt (FILE* file_output, int n_str, struct Line* mystr)
     fprintf(file_output, "\n");
 
 }
-int compare_begin (struct Line *stract1, struct Line *stract2)
+int compare_begin (const struct Line *stract1, const struct Line *stract2)
 {
-    int elem_num_one = 0, elem_num_two = 0;
 
-    while (!isalpha(*( (stract1->str) + elem_num_one)) && ((stract1->len) > elem_num_one))
-    {
-        elem_num_one++;
-    }
-    while (!isalpha(*( (stract2->str) + elem_num_two)) && ((stract2->len) > elem_num_two))
-    {
-        elem_num_two++;
-    }
-    if ((elem_num_one >= (stract1->len)) || (elem_num_two >= (stract2->len)))
-    {
-        return -((elem_num_one >= (stract1->len)) - (elem_num_two >= (stract2->len)));
-    }
-    while (*( (stract1->str) + elem_num_one) ==  *( (stract2->str) + elem_num_two))
+    int elem_num_one = -1, elem_num_two = -1;
+    do
     {
         elem_num_one++;
         elem_num_two++;
-
         while (!isalpha(*( (stract1->str) + elem_num_one)) && ((stract1->len) > elem_num_one))
         {
             elem_num_one++;
@@ -158,28 +127,18 @@ int compare_begin (struct Line *stract1, struct Line *stract2)
         {
             return -((elem_num_one >= (stract1->len)) - (elem_num_two >= (stract2->len)));
         }
-    }
+    } while (*( (stract1->str) + elem_num_one) ==  *( (stract2->str) + elem_num_two));// ToDo: do while
 
-    return *( (stract1->str) + elem_num_one) - *( (stract2->str) + elem_num_two);
+    return *( (stract1->str) + elem_num_one) - *( (stract2->str) + elem_num_two); // ToDo: []
 }
 
-int compare_end (struct Line *stract1, struct Line *stract2)
+int compare_end (const struct Line *stract1, const struct Line *stract2)
 {
-    int elem_num_one = 0, elem_num_two = 0;
 
-    while (!isalpha(*( (stract1->str) + (stract1->len) - 1 - elem_num_one)) && (stract1->len > elem_num_one))
-    {
-        elem_num_one++;
-    }
-    while (!isalpha(*( (stract2->str) + (stract2->len) - 1 - elem_num_two)) && (stract2->len > elem_num_two))
-    {
-        elem_num_two++;
-    }
-    if ((elem_num_one >= (stract1->len) )|| (elem_num_two >= (stract2->len)))
-    {
-        return -((elem_num_one >= (stract1->len)) - (elem_num_two >= (stract2->len)));
-    }
-    while ( *( (stract1->str) + (stract1->len) - 1 - elem_num_one) ==  *( (stract2->str) + (stract2->len) - 1 - elem_num_two))
+    int elem_num_one = -1, elem_num_two = -1;
+
+
+    do
     {
         elem_num_one++;
         elem_num_two++;
@@ -194,9 +153,10 @@ int compare_end (struct Line *stract1, struct Line *stract2)
         }
         if ((elem_num_one >= (stract1->len)) || (elem_num_two >= (stract2->len)))
         {
-            return -((elem_num_one >= (stract1->len)) - (elem_num_two >= (stract2->len)));
+            return -(elem_num_one >= (stract1->len) ) + (elem_num_two >= (stract2->len) );
         }
-    }
+    } while ( *( (stract1->str) + (stract1->len) - 1 - elem_num_one) ==  *( (stract2->str) + (stract2->len) - 1 - elem_num_two));
 
     return *((stract1->str) + (stract1->len) - 1 - elem_num_one) -  *((stract2->str) + (stract2->len) - 1 - elem_num_two);
 }
+
