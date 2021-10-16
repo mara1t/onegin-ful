7199143d0fb6a52f985adc7267a28fe5248aa249
mara1t <88665544+mara1t@users.noreply.github.com>
2021-10-16 12:45:50 +0300

    Update header_og.h

--- header_og.h
+++ header_og.h
@@ -1,18 +1,19 @@
-struct Line {
+#include <stdio.h>
+struct Line
+{
     int len;
     char* str;
 };
-char* file_read        (const int file_size, const FILE* og);
+char* file_read        (const int file_size, FILE* og);
 
-int   File_size        (const FILE* og);
+int   File_size        (FILE* og);
 int   Nstr             (const int file_size, const char *buffer);
-
 void  line_pointer_len (const int file_size, char *buffer, struct Line* mystr);
 
-int   compare_begin    (const struct Line *stract1, const struct Line *stract2);
-int   compare_end      (const struct Line *stract1, const struct Line *stract2);
+int   compare_begin    (const void* stract1, const void* stract2); //(const struct Line* stract1, const struct Line* stract2);
+int   compare_end      (const void* stract1, const void* stract2);
 
 void  free_member      (struct Line* mystr, char *buffer, FILE* file_onegin, FILE* file_output);
-void  output_txt       (const FILE* file_output, const int n_str, const struct Line* mystr);
+void  output_txt       (FILE* file_output, const int n_str, const struct Line* mystr);
 
 
